6ddf50ba01183cb17c65bd3912908efd51727f05
karthi <karthirajendran12003@gmail.com>
2022-06-07 19:09:32 +0530

    req

--- models/User.js
+++ models/User.js
@@ -4,21 +4,16 @@ const {Schema} = mongoose
 const userSchema = new Schema({
     name:{
         type:String,
-        required:true
     },
     email:{
         type:String,
-        required:true,
-        unique:true
     },
     phone:{
         type:Number,
-        required:true,
-        unique:true,
+        
     },
     password:{
         type:String,
-        required:true,
     },
     // location:{
     //     type:String,
